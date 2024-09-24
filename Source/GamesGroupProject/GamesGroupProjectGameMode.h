7a78848cb9c0387da426ee6f917ee1aa939df000
Marcos Somoza <somozadev@gmail.com>
2024-09-24 18:17:28 +0100

    Initial Commit

---
+++ Source/GamesGroupProject/GamesGroupProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GamesGroupProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGamesGroupProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGamesGroupProjectGameMode();
+};
+
+
+
