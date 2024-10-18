411902ed84bf42f6709164e995701eabbe1ee9c0
Sara Rushmer <57770253+SaraR-GameProgramming@users.noreply.github.com>
2024-10-18 14:42:07 +0100

    New Player Cube
    created BP for the new player cube and a test game mode caled BPG_TestGameMode,
    added jump to blueprint for designers (on spacebar press for now)
    finished movement and rotation code in playerCube.cc & .h
    moved defualt health into public from protected for dan to use with AI damage calls.

--- Source/GamesGroupProject/Public/PlayerCube.h
+++ Source/GamesGroupProject/Public/PlayerCube.h
@@ -4,7 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "PlayerHealthComponent.h"
 #include "PlayerCube.generated.h"
+
+
+
+class USpringArmComponent;
+class UCameraComponent;
+class UStaticMeshComponent;
+class UInputAction;
 
 UCLASS()
 class GAMESGROUPPROJECT_API APlayerCube : public ACharacter
@@ -15,9 +23,26 @@ public:
 	// Sets default values for this character's properties
 	APlayerCube();
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Camera")
+	class USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Camera")
+	class UCameraComponent* CameraComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "PlayerCube")
+	UStaticMeshComponent* PlayerMeshComponent;
+
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void TurningRate(float Value);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Camera")
+	float DefaultTurningRate;
 
 public:	
 	// Called every frame
