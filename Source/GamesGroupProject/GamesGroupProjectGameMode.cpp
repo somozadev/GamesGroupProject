0151e8c9431e62a227f9842a1a18c8b0348b965a
Marcos Somoza <somozadev@gmail.com>
2024-10-21 10:28:58 +0100

    Revert "Events manager working"

--- Source/GamesGroupProject/GamesGroupProjectGameMode.cpp
+++ Source/GamesGroupProject/GamesGroupProjectGameMode.cpp
@@ -7,7 +7,7 @@
 AGamesGroupProjectGameMode::AGamesGroupProjectGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/GroupProject/Player/BP_PlayerCube"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
