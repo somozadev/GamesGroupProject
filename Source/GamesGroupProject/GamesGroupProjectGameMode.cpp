53f6e521bd2a59b65f4010c60a35efa019196d1c
maya <somozadev@gmail.com>
2024-11-14 18:17:50 +0000

    Revert "Merge pull request #43 from somozadev/Sujin_Puzzle"
    This reverts commit 9100eb60ea2883b0e529991cc4c00a917b5b7b72, reversing
    changes made to 0e3add4ba3d1f5dc9df9a9ffee312cf65a6fdc1f.

--- Source/GamesGroupProject/GamesGroupProjectGameMode.cpp
+++ Source/GamesGroupProject/GamesGroupProjectGameMode.cpp
@@ -7,11 +7,7 @@
 AGamesGroupProjectGameMode::AGamesGroupProjectGameMode()
 {
 	// set default pawn class to our Blueprinted character
-<<<<<<< HEAD
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/GroupProject/Player/BP_NewPlayerCube"));
-=======
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
->>>>>>> main
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
