406f16e333961f3df611e1c97086c4c2373ba4f6
somozadev <somozadev@gmail.com>
2024-11-17 13:50:05 +0000

    Lava attack working
    Inputs -> Place inputs for it and call aim on card selected and use attack on shoot input
    + Can't rotate player to target when aiming cuz Z rotation is hard locked with mouse/cam
    Object pooling -> for the lavasplash instantiatiate and delete, not sure how to proceed with the current obj pooling

---
+++ Source/GamesGroupProject/Public/LavaAttackSplash.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "LavaAttackSplash.generated.h"
+class AAIEnemy;
+
+UCLASS()
+class GAMESGROUPPROJECT_API ALavaAttackSplash : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ALavaAttackSplash();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USceneComponent* DefaultSceneRoot;
+	
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category="Default")
+	TObjectPtr<UStaticMeshComponent> Visuals;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category="Default")
+	TObjectPtr<USphereComponent> DamageArea;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Default")
+	double SplashDuration =4.5f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Default")
+	double SplashTickDamageDuration = 2.0f;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category="Default")
+	bool EnemyInSplash;
+
+	UPROPERTY(BlueprintReadOnly, EditInstanceOnly, Category="Default")
+	 TArray<AAIEnemy*> EnemiesInSplash;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Default")
+	int32 Damage = 1.0f;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	                    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+	                    bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	                  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+private:
+	double ElapsedSplashDuration;
+	double ElapsedToTickDmgSplashDuration;
+};
