b00f2eeec47f5abca6b11d542a5a4d806b8bf9f2
somozadev <somozadev@gmail.com>
2025-01-05 15:09:52 +0100

    Removed and fixed huge amount of stuff and bps with errors so game would build
    Although inventory and ui wheel not working as expected by any means
    
    Build done in game's current state, available at: https://drive.google.com/file/d/1_dJp8-Nt9Dqw_BdVnoxcY9sT9gPrrWHj/view?usp=sharing

--- Source/GamesGroupProject/GamesGroupProject.Build.cs
+++ Source/GamesGroupProject/GamesGroupProject.Build.cs
@@ -7,7 +7,7 @@ public class GamesGroupProject : ModuleRules
 	public GamesGroupProject(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-
+		bEnableExceptions = true;
 		PublicDependencyModuleNames.AddRange(new string[]
 		{
 			"Core", "CoreUObject", "Engine", "InputCore",
